e29b9bbfab6baf76247c10bb930c7ad6ca04d86f
Drew Pentland <drewtpentland@gmail.com>
2023-02-11 19:51:03 +0100

    Things are set up, but little coding done. Still need to figure out how to organize things.

--- push_swap_working/srcs/push_swap.c
+++ push_swap_working/srcs/push_swap.c
@@ -12,8 +12,27 @@
 
 #include "push_swap.h"
 
+/*	*** error_and_close (error and close) ***
+ *
+ *	
+ */
+
+int	error_and_close()
+{
+
+	return (0);
+}
+
+/* *** main (push swap) ***
+ *
+ *	Entry point for push_swap program.
+ */
+
 int main (int argc, char **argv)
 {
+	if (argc == 1)
+	
+	ft_printf("%s\n", get_next_line(0));
 	ft_printf("argc = %d\n", argc);
 	ft_printf("argv = %p\n", argv);
 
